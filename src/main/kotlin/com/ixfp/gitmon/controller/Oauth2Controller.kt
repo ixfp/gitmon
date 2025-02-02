7b16d8eee89a6137240f459b4112b08abe6163a0
TraceofLight <rickyjun96@gmail.com>
2025-02-02 18:31:44 +0900

    feat: springdoc sample
    문서화 라이브러리 활용을 위한 샘플 추가
    - auth controller에 annotation 적용

--- src/main/kotlin/com/ixfp/gitmon/controller/Oauth2Controller.kt
+++ src/main/kotlin/com/ixfp/gitmon/controller/Oauth2Controller.kt
@@ -4,8 +4,16 @@ import com.ixfp.gitmon.controller.request.GithubOauth2Request
 import com.ixfp.gitmon.controller.response.AccessTokenResponse
 import com.ixfp.gitmon.domain.auth.AuthService
 import io.github.oshai.kotlinlogging.KotlinLogging.logger
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,10 +24,22 @@ import javax.naming.AuthenticationException
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name = "OAuth Token Control", description = "OAuth 인증 관련 API")
 class Oauth2Controller(
     @Value("\${oauth2.client.github.id}") private val githubClientId: String,
     private val authService: AuthService,
 ) {
+    @Operation(summary = "Get Oauth2 Token", description = "Github으로부터 Oauth2 Token을 받아오는 API")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200", description = "Success", content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = ArraySchema(schema = Schema(implementation = String::class))
+                )]
+            ),
+        ]
+    )
     @GetMapping("/login/oauth/github")
     fun redirectToGithubOauthUrl(): ResponseEntity<Unit> {
         return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).header(
@@ -35,16 +55,14 @@ class Oauth2Controller(
         try {
             val accessToken = authService.createAccessToken(request.code)
             val response = AccessTokenResponse(accessToken)
-            return ResponseEntity.status(HttpStatus.CREATED)
-                .body(response)
+            return ResponseEntity.status(HttpStatus.CREATED).body(response)
         } catch (e: Exception) {
             log.info { "Failed to login: ${e.message}" }
-            val status =
-                when (e) {
-                    is IllegalArgumentException -> HttpStatus.BAD_REQUEST
-                    is AuthenticationException -> HttpStatus.UNAUTHORIZED
-                    else -> HttpStatus.INTERNAL_SERVER_ERROR
-                }
+            val status = when (e) {
+                is IllegalArgumentException -> HttpStatus.BAD_REQUEST
+                is AuthenticationException -> HttpStatus.UNAUTHORIZED
+                else -> HttpStatus.INTERNAL_SERVER_ERROR
+            }
             return ResponseEntity.status(status).build()
         }
     }
