83e27019ec99cba8df62496d633c04ea4b577217
조영우 <choyoungwoo9@naver.com>
2025-02-21 22:44:48 +0900

    feat: 회원가입 서비스 메서드 구현

--- src/main/kotlin/com/ixfp/gitmon/domain/auth/AuthService.kt
+++ src/main/kotlin/com/ixfp/gitmon/domain/auth/AuthService.kt
@@ -1,15 +1,22 @@
 package com.ixfp.gitmon.domain.auth
 
 import com.ixfp.gitmon.client.github.GithubApiService
+import com.ixfp.gitmon.client.github.GithubUserResponse
 import com.ixfp.gitmon.common.util.JwtUtil
+import com.ixfp.gitmon.db.github.GithubAuthEntity
+import com.ixfp.gitmon.db.github.GithubAuthRepository
+import com.ixfp.gitmon.db.member.MemberEntity
 import com.ixfp.gitmon.db.member.MemberRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class AuthService(
     private val githubApiService: GithubApiService,
     private val jwtUtil: JwtUtil,
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val githubAuthRepository: GithubAuthRepository
 ) {
     fun isMember(githubAccessToken: String): Boolean {
         val githubUser = githubApiService.getGithubUser(githubAccessToken)
@@ -17,12 +24,22 @@ class AuthService(
         return member != null
     }
 
-    // TODO: 회원가입
-    fun signup() {
-        
+    @Transactional
+    fun signup(githubAccessToken: String, githubUser: GithubUserResponse) {
+        val memberEntity = MemberEntity(
+            exposedId = UUID.randomUUID().toString(),
+            githubId = githubUser.id.toLong(),
+            githubUsername = githubUser.username,
+        )
+        val savedMember = memberRepository.save(memberEntity)
+        val githubAuthEntity = GithubAuthEntity(savedMember, githubAccessToken)
+        val savedGithubAuth = githubAuthRepository.save(githubAuthEntity)
     }
 
     // TODO: 로그인
+    fun login(githubAccessToken: String, githubUser: GithubUserResponse) {
+
+    }
 
     fun createAccessToken(code: String): String {
         val user = User.from(githubApiService.getGithubUser(code))
