7a10b5c8b52762e1825d09c980536e6c31d5b08e
조영우 <choyoungwoo9@naver.com>
2025-02-21 22:43:03 +0900

    feat: GithubAuthRepository, MemberRepository에 save 메서드 추가

--- src/main/kotlin/com/ixfp/gitmon/db/member/MemberRepository.kt
+++ src/main/kotlin/com/ixfp/gitmon/db/member/MemberRepository.kt
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.Optional
 
 interface MemberRepository : JpaRepository<MemberEntity, Long> {
+    override fun <S : MemberEntity> save(entity: S): S
+
     override fun findById(memberId: Long): Optional<MemberEntity>
 
     fun findByGithubId(githubId: Long): MemberEntity?
