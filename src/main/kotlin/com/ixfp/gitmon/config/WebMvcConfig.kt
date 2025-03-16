edcad3186fc15a8460f97b823f4b127090ea8c87
Suhjeong Kim <kimsj.py@gmail.com>
2025-03-16 19:34:30 +0900

    CORS 설정
    Signed-off-by: Suhjeong Kim <kimsj.py@gmail.com>

--- src/main/kotlin/com/ixfp/gitmon/config/WebMvcConfig.kt
+++ src/main/kotlin/com/ixfp/gitmon/config/WebMvcConfig.kt
@@ -1,6 +1,7 @@
 package com.ixfp.gitmon.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -11,5 +12,19 @@ class WebMvcConfig(
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(jwtAuthInterceptor)
             .addPathPatterns("/api/v1/member/repo/**") // 인증이 필요한 경로
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(*ALLOWED_ORIGINS)
+            .allowedMethods(*ALLOWED_METHODS)
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+
+    companion object {
+        private val GITMON_CLIENT_URL = "https://gitmon.blog"
+        private val ALLOWED_ORIGINS = arrayOf(GITMON_CLIENT_URL)
+        private val ALLOWED_METHODS = arrayOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
     }
 }
