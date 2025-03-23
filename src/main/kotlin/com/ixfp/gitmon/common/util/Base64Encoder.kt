35481523a57c14f52f760f2c75f1a62b531756ea
Suhjeong Kim <kimsj.py@gmail.com>
2025-03-23 21:36:04 +0900

    base64 standard encoder 적용
    Signed-off-by: Suhjeong Kim <kimsj.py@gmail.com>

--- src/main/kotlin/com/ixfp/gitmon/common/util/Base64Encoder.kt
+++ src/main/kotlin/com/ixfp/gitmon/common/util/Base64Encoder.kt
@@ -5,6 +5,6 @@ import java.util.Base64
 
 object Base64Encoder {
     fun encodeBase64(file: MultipartFile): String {
-        return Base64.getUrlEncoder().encodeToString(file.bytes)
+        return Base64.getEncoder().encodeToString(file.bytes)
     }
 }
