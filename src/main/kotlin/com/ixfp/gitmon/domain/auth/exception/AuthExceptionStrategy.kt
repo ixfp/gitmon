cf8584889bf9e67835a8970383f9a6f6b9eb0bb5
조영우 <choyoungwoo9@naver.com>
2025-05-09 10:36:58 +0900

    chore: 누락된 커밋

--- src/main/kotlin/com/ixfp/gitmon/domain/auth/exception/AuthExceptionStrategy.kt
+++ src/main/kotlin/com/ixfp/gitmon/domain/auth/exception/AuthExceptionStrategy.kt
@@ -2,7 +2,9 @@ package com.ixfp.gitmon.domain.auth.exception
 
 import com.ixfp.gitmon.common.aop.ExceptionWrappingStrategy
 import com.ixfp.gitmon.common.exception.AppException
+import org.springframework.stereotype.Component
 
+@Component
 class AuthExceptionStrategy : ExceptionWrappingStrategy {
     override fun wrap(e: Throwable): AppException {
         return when (e) {
